97f5fc8f039ca2aedd0f9ab586585a013a88c4bd
unknown <kevinmcpe537@gmail.com>
2021-02-20 08:19:54 -0500

    commit

--- comandos.js
+++ comandos.js
@@ -1,47 +1,12 @@
 var command = function(client) {
     const fs = require('fs')
-
-const variados = fs.readdirSync('./comandos/variados').filter(file => file.endsWith('.js')) 
-const interaccion = fs.readdirSync('./comandos/interaccion&reaccion').filter(file => file.endsWith('.js')) 
-const moderacion = fs.readdirSync('./comandos/moderacion').filter(file => file.endsWith('.js')) 
-const sorteos = fs.readdirSync('./comandos/sorteos').filter(file => file.endsWith('.js'))  
-const diversion = fs.readdirSync('./comandos/diversion').filter(file => file.endsWith('.js'))
-const dev = fs.readdirSync('./comandos/developer').filter(file => file.endsWith('.js'))
-const musica = fs.readdirSync('./comandos/musica').filter(file => file.endsWith('.js'))
-
-for (const file of variados){
-    const  obtenerComando = require(`./comandos/variados/${file}`)
-    client.comandos.set(obtenerComando.name, obtenerComando)
-
-}
-for (const file of interaccion){
-    const  obtenerComando = require(`./comandos/interaccion&reaccion/${file}`)
- client.comandos.set(obtenerComando.name, obtenerComando)
-}
-
-for (const file of moderacion){
-    const  obtenerComando = require(`./comandos/moderacion/${file}`)
-client.comandos.set(obtenerComando.name, obtenerComando)
-}
-for (const file of sorteos){
-    const  obtenerComando = require(`./comandos/sorteos/${file}`)
- client.comandos.set(obtenerComando.name, obtenerComando)
-}
-
-for (const file of diversion){
-    const  obtenerComando = require(`./comandos/diversion/${file}`)
- client.comandos.set(obtenerComando.name, obtenerComando)
-}
-for (const file of dev){
-    const  obtenerComando = require(`./comandos/developer/${file}`)
- client.comandos.set(obtenerComando.name, obtenerComando)
-}
-
-for (const file of musica){
-    const  obtenerComando = require(`./comandos/musica/${file}`)
- client.comandos.set(obtenerComando.name, obtenerComando)
-}
-
+fs.readdirSync("./comandos").forEach(carpeta => {
+        const commands = fs.readdirSync(`./comandos/${carpeta}/`).filter(f => f.endsWith(".js"));
+        for(file of commands) {
+          const comando = require(`./comandos/${carpeta}/${file}`);
+          client.comandos.set(comando.name, comando);
+        };
+      });
 }
 module.exports = { command }
 
