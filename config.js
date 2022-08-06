6a3b2160259cfd614e3f3b02017189be2b9783df
Urlesst <73747877+Urlesst@users.noreply.github.com>
2022-08-06 13:12:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    token: 'Nzc4NTg4OTg4MjYzODkwOTU0.X7ULgA.bsONXVvyh0GFqP5j-1Ino7q7tQQ',
-    mongo: 'mongodb+srv://kevinshidori:MinecraftxD@mute.cwzil.mongodb.net/xd?retryWrites=true&w=majority',
-    youtube: 'AIzaSyBwzVwDTT-iPSLXUH7pXAdZBmGNQ10g1b4'
+    token: 'insert token',
+    mongo: 'inset mongo',
+    youtube: 'a'
 }
 module.exports = config;
